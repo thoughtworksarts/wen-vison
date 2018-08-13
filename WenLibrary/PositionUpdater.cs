2d15b39ff1347196bfbe6f02335873db3fe50a4b
Andy <aallen@thoughtworks.com>
2018-08-13 12:21:00 -0700

    Andy | Adding basic PositionUpdater class to turn rotation angles into x,y,z coordinates

---
+++ WenLibrary/PositionUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+public class PositionUpdater
+{
+	public PositionUpdater()
+	{
+	}
+
+    //uses last array of positions for each joint (starting position)
+
+    //takes in the array of degrees from the parser
+
+    //should generate a list of positions in x,y,z coords for each joint 
+
+    //returns the new positions so that we can then translate them to the depth points and visualize them
+}
